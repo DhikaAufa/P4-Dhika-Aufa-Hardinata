dbfdade704097718b07ab33c55940d4c9339ff9d
DhikaAufa <64623048+DhikaAufa@users.noreply.github.com>
2020-05-15 14:46:20 +0700

    Create Jobsheet 1 C.cpp
    Menampilkan Array satu dimensi

---
+++ Jobsheet 1 C.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main(){
+	
+	int myarray[10] = {1,3,5,7,9,2,4,6,8,};
+	
+	printf("\nNAMA		: Dhika Aufa Hardinata");
+	printf("\nNIM		: F1B019039");
+	printf("\nKELOMPOK	: 8");
+	printf("\ndata ke-1 = %d" ,myarray[0]);
+	printf("\ndata ke-2 = %d" ,myarray[1]);
+	printf("\ndata ke-3 = %d" ,myarray[2]);
+	printf("\ndata ke-4 = %d" ,myarray[3]);
+	printf("\ndata ke-5 = %d" ,myarray[4]);
+	printf("\ndata ke-6 = %d" ,myarray[5]);
+	printf("\ndata ke-7 = %d" ,myarray[6]);
+	printf("\ndata ke-8 = %d" ,myarray[7]);
+	printf("\ndata ke-9 = %d" ,myarray[8]);
+
+}
